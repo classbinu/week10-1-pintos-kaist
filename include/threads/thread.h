c4052bda0f2c25c021949be2cb477f55a889542b
classbinu <classbinu@gmail.com>
2023-12-14 06:02:14 +0000

    first commit

---
+++ include/threads/thread.h
@@ -0,0 +1,202 @@
+#ifndef THREADS_THREAD_H
+#define THREADS_THREAD_H
+
+#include <debug.h>
+#include <list.h>
+#include <stdint.h>
+#include "threads/synch.h"
+#include "threads/interrupt.h"
+#ifdef VM
+#include "vm/vm.h"
+#endif
+
+
+/* States in a thread's life cycle. */
+enum thread_status {
+	THREAD_RUNNING,     /* Running thread. */
+	THREAD_READY,       /* Not running but ready to run. */
+	THREAD_BLOCKED,     /* Waiting for an event to trigger. */
+	THREAD_DYING        /* About to be destroyed. */
+};
+
+/* Thread identifier type.
+   You can redefine this to whatever type you like. */
+typedef int tid_t;
+#define TID_ERROR ((tid_t) -1)          /* Error value for tid_t. */
+
+/* Thread priorities. */
+#define PRI_MIN 0                       /* Lowest priority. */
+#define PRI_DEFAULT 31                  /* Default priority. */
+#define PRI_MAX 63                      /* Highest priority. */
+#define NICE_MIN -20
+#define NICE_DEFAULT 0
+#define NICE_MAX 20
+#define RECENT_CPU_DEFAULT 0
+#define LOAD_AVG_DEFAULT 0
+
+/* system call */
+#define FDT_PAGES 3
+#define FDCOUNT_LIMIT FDT_PAGES *(1 << 9)
+
+/* A kernel thread or user process.
+ *
+ * Each thread structure is stored in its own 4 kB page.  The
+ * thread structure itself sits at the very bottom of the page
+ * (at offset 0).  The rest of the page is reserved for the
+ * thread's kernel stack, which grows downward from the top of
+ * the page (at offset 4 kB).  Here's an illustration:
+ *
+ *      4 kB +---------------------------------+
+ *           |          kernel stack           |
+ *           |                |                |
+ *           |                |                |
+ *           |                V                |
+ *           |         grows downward          |
+ *           |                                 |
+ *           |                                 |
+ *           |                                 |
+ *           |                                 |
+ *           |                                 |
+ *           |                                 |
+ *           |                                 |
+ *           |                                 |
+ *           +---------------------------------+
+ *           |              magic              |
+ *           |            intr_frame           |
+ *           |                :                |
+ *           |                :                |
+ *           |               name              |
+ *           |              status             |
+ *      0 kB +---------------------------------+
+ *
+ * The upshot of this is twofold:
+ *
+ *    1. First, `struct thread' must not be allowed to grow too
+ *       big.  If it does, then there will not be enough room for
+ *       the kernel stack.  Our base `struct thread' is only a
+ *       few bytes in size.  It probably should stay well under 1
+ *       kB.
+ *
+ *    2. Second, kernel stacks must not be allowed to grow too
+ *       large.  If a stack overflows, it will corrupt the thread
+ *       state.  Thus, kernel functions should not allocate large
+ *       structures or arrays as non-static local variables.  Use
+ *       dynamic allocation with malloc() or palloc_get_page()
+ *       instead.
+ *
+ * The first symptom of either of these problems will probably be
+ * an assertion failure in thread_current(), which checks that
+ * the `magic' member of the running thread's `struct thread' is
+ * set to THREAD_MAGIC.  Stack overflow will normally change this
+ * value, triggering the assertion. */
+/* The `elem' member has a dual purpose.  It can be an element in
+ * the run queue (thread.c), or it can be an element in a
+ * semaphore wait list (synch.c).  It can be used these two ways
+ * only because they are mutually exclusive: only a thread in the
+ * ready state is on the run queue, whereas only a thread in the
+ * blocked state is on a semaphore wait list. */
+struct thread {
+	/* Owned by thread.c. */
+	tid_t tid;                          /* Thread identifier. */
+	enum thread_status status;          /* Thread state. */
+	char name[16];                      /* Name (for debugging purposes). */
+	int priority;                       /* Priority. */
+	
+
+	/* Shared between thread.c and synch.c. */
+	struct list_elem elem;              /* List element. */
+
+	/* 깨어나야 할 틱 저장 */
+	int64_t wake_up_ticks;
+
+	/* Priority donation */
+	int original_priority;				/* boost 이전의 priority */
+	struct lock *waiting_lock;			/* 이 스레드가 사용을 기다리고 있는 락 */
+	struct list donations;				
+	struct list_elem donations_elem;
+
+	/* Advanced Scheduler */
+	int nice;
+	int recent_cpu;
+	
+	/* process */
+	struct list child_list;
+	struct list_elem child_elem;
+	struct semaphore wait_sema;
+	struct semaphore fork_sema;
+	struct semaphore exit_sema;
+	int exit_status;
+	struct intr_frame parent_tf;
+
+#ifdef USERPROG
+	/* Owned by userprog/process.c. */
+	uint64_t *pml4; /* Page map level 4 */
+#endif
+#ifdef VM
+	/* Table for whole virtual memory owned by thread. */
+	struct supplemental_page_table spt;
+#endif
+
+	/* Owned by thread.c. */
+	struct intr_frame tf;               /* Information for switching */
+	unsigned magic;                     /* Detects stack overflow. */
+
+	/* filesys */
+	struct file **fd_table;
+	int fd_idx;
+	struct file *running;
+};
+
+/* If false (default), use round-robin scheduler.
+   If true, use multi-level feedback queue scheduler.
+   Controlled by kernel command-line option "-o mlfqs". */
+extern bool thread_mlfqs;
+
+void thread_init (void);
+void thread_start (void);
+
+void thread_tick (void);
+void thread_print_stats (void);
+
+typedef void thread_func (void *aux);
+tid_t thread_create (const char *name, int priority, thread_func *, void *);
+
+
+void thread_block (void);
+bool priority_more (const struct list_elem *a_, const struct list_elem *b_, void *aux UNUSED);
+void test_max_priority(void);
+void thread_unblock (struct thread *);
+void thread_awake(int64_t ticks);
+
+
+struct thread *thread_current (void);
+tid_t thread_tid (void);
+const char *thread_name (void);
+
+void thread_exit (void) NO_RETURN;
+void thread_yield (void);
+
+int thread_get_priority (void);
+void thread_set_priority (int);
+
+void refresh_priority (void);
+void donate_priority (void);
+void remove_with_lock (struct lock *lock);
+
+/* MLFQS */
+int thread_get_nice (void);
+void thread_set_nice (int);
+int thread_get_recent_cpu (void);
+int thread_get_load_avg (void);
+
+void mlfqs_priority (struct thread *t);
+void mlfqs_recent_cpu (struct thread *t);
+void mlfqs_load_avg (void);
+void mlfqs_increment (void);
+void mlfqs_recalc (void);
+
+void do_iret (struct intr_frame *tf);
+struct thread *get_child_thread (tid_t tid);
+
+
+#endif /* threads/thread.h */
