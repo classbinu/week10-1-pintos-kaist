3c173d0edd525b1bc2b43b35fc6a5d7944b1ac2f
classbinu <classbinu@gmail.com>
2023-12-22 06:26:00 +0000

    메모리 관리 2차 구현

--- include/vm/vm.h
+++ include/vm/vm.h
@@ -48,6 +48,7 @@ struct page {
 
 	/* Your implementation */
 	struct hash_elem hash_elem;
+	bool writable;
 
 	/* Per-type data are binded into the union.
 	 * Each function automatically detects the current union */
@@ -87,7 +88,7 @@ struct page_operations {
  * We don't want to force you to obey any specific design for this struct.
  * All designs up to you for this. */
 struct supplemental_page_table {
-	struct hash pages;
+	struct hash spt_hash
 };
 
 #include "threads/thread.h"
